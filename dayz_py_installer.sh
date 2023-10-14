84193f87abeb56bff834d98dae023b2c440db8b2
1 <dallas6672@msn.com>
2023-10-14 01:30:55 +0000

    Upload installer.sh

---
+++ dayz_py_installer.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Set paths
+desktopFile="$HOME/.local/share/applications/dayz_py.desktop"
+share="$HOME/.local/share/dayz_py"
+dayz_py_file="$share/dayz_py_launcher.py"
+tarDownload="https://gitlab.com/tenpenny/dayz-py-launcher/-/archive/main/dayz-py-launcher-main.tar.gz"
+
+# Create the required directories
+mkdir -p "$share"
+
+# Download the dzgui.sh script and save it to the desired location
+curl -L $tarDownload | tar zxf - --strip-components=1 -C "$share"
+chmod +x "$dayz_py_file"
+
+# Write the content to the desktopFile
+cat <<-END > "$desktopFile"
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=python $dayz_py_file
+Name=DayZ Py Launcher
+Path=$share
+Comment=DayZ Py Launcher
+Icon=$share/dayz_icon.png
+Categories=Game
+END
