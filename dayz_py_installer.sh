2a10d9b2742ed2d7a6e4f9646335aad569dd6842
1 <dallas6672@msn.com>
2023-10-24 14:15:03 +0000

    Update dayz_py_installer.sh

--- dayz_py_installer.sh
+++ dayz_py_installer.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-# Use DZGUI's check for Steam Deck (https://github.com/aclist/dztui)
+# Use DZGUI's check for Steam Deck & write desktop function(https://github.com/aclist/dztui)
 cpu=$(cat /proc/cpuinfo | grep "AMD Custom APU 0405")
 if [[ -n "$cpu" ]]; then
     is_steam_deck=1
 else
     is_steam_deck=0
+
+write_desktop_file(){
+cat <<-END
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=python $dayz_py_file
+Name=DayZ Py Launcher
+Path=$share
+Comment=DayZ Py Launcher
+Icon=$share/dayz_icon.png
+Categories=Game
+	END
+}
 
 # Set paths
 desktopFile="$HOME/.local/share/applications/dayz_py.desktop"
@@ -20,31 +35,7 @@ mkdir -p "$share"
 curl -L $tarDownload | tar zxf - --strip-components=1 -C "$share"
 
 # Write the content to the desktopFile
+write_desktop_file > "$desktopFile"
 if [[ $is_steam_deck -eq 1 ]]; then
-cat <<-END > "$HOME/Desktop/dayz_py.desktop"
-[Desktop Entry]
-Version=1.0
-Type=Application
-Terminal=false
-Exec=python $dayz_py_file
-Name=DayZ Py Launcher
-Path=$share
-Comment=DayZ Py Launcher
-Icon=$share/dayz_icon.png
-Categories=Game
-	END
+    write_desktop_file > "$HOME/Desktop/dzgui.desktop"
 fi
-
-[[ -f $desktopFile ]] && rm $desktopFile
-cat <<-END > "$desktopFile"
-[Desktop Entry]
-Version=1.0
-Type=Application
-Terminal=false
-Exec=python $dayz_py_file
-Name=DayZ Py Launcher
-Path=$share
-Comment=DayZ Py Launcher
-Icon=$share/dayz_icon.png
-Categories=Game
-	END
