2f0a1b8ee12d1e1a6f69ed7dfdf5ac271634b8bb
1 <dallas6672@msn.com>
2023-10-31 12:54:15 +0000

    Remove Steam Deck desktop shortcut

--- dayz_py_installer.sh
+++ dayz_py_installer.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-# Use DZGUI's check for Steam Deck & write desktop function(https://github.com/aclist/dztui)
-cpu=$(cat /proc/cpuinfo | grep "AMD Custom APU 0405")
-if [[ -n "$cpu" ]]; then
-    is_steam_deck=1
-else
-    is_steam_deck=0
-fi
-
-write_desktop_file(){
-cat <<-END
-[Desktop Entry]
-Version=1.0
-Type=Application
-Terminal=false
-Exec=python $dayz_py_file
-Name=DayZ Py Launcher
-Path=$share
-Comment=DayZ Py Launcher
-Icon=$share/dayz_icon.png
-Categories=Game
-	END
-}
-
 # Set paths
 desktopFile="$HOME/.local/share/applications/dayz_py.desktop"
 share="$HOME/.local/share/dayz_py"
@@ -36,7 +13,16 @@ mkdir -p "$share"
 curl -L $tarDownload | tar zxf - --strip-components=1 -C "$share"
 
 # Write the content to the desktopFile
-write_desktop_file > "$desktopFile"
-if [[ $is_steam_deck -eq 1 ]]; then
-    write_desktop_file > "$HOME/Desktop/dayz_py.desktop"
-fi
+[[ -f $desktopFile ]] && rm $desktopFile
+cat <<-END > "$desktopFile"
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=python $dayz_py_file
+Name=DayZ Py Launcher
+Path=$share
+Comment=DayZ Py Launcher
+Icon=$share/dayz_icon.png
+Categories=Game
+END
